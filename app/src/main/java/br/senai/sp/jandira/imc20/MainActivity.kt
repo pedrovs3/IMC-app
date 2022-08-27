551bd98be3de076d626d90e2ab657d6d47c4f940
Pedro <pedrovs3@hotmail.com>
2022-08-27 17:25:28 -0300

    activity calculator design

--- app/src/main/java/br/senai/sp/jandira/imc20/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/imc20/MainActivity.kt
@@ -3,10 +3,12 @@ package br.senai.sp.jandira.imc20
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var textSignup: TextView
+    lateinit var buttonLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +20,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(openSignupActivity)
         }
         supportActionBar!!.hide()
+
+//        buttonLogin = findViewById(R.id.buttonLogin)
+//        buttonLogin.setOnClickListener {
+//            val openCalculatorActivity = Intent(this, CalculatorActivity::class.java)
+//            startActivity(openCalculatorActivity)
+//        }
     }
 }
